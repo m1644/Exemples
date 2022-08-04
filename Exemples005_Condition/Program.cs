9f01c7a4dfe3e0ba8cce75cc7b0ede305c24255a
MaxX <maxx-f@mail.ru>
2022-08-04 08:25:45 +0300

    Add Exemples005 Condition

---
+++ Exemples005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "max")
+{
+    Console.WriteLine("Ура, это же Max!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
