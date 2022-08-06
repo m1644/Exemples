1efdef68860d923da7a6fa6e5db42f9eb0e83ba5
MaxX <maxx-f@mail.ru>
2022-08-06 16:44:50 +0300

    Add Ex010

---
+++ Ex010/Program.cs
@@ -0,0 +1,17 @@
+﻿// Имеется одномерный массив array из n элементов,
+// требуется найти элемент массива, равный find...
+
+int[] array = {1, 4, 8, 16, 24, 44, 18, 22, 44};
+int n = array.Length;
+int find = 44;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
