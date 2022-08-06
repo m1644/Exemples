ff8420c85acbd92df17efb8af13619bfe2a9231a
MaxX <maxx-f@mail.ru>
2022-08-06 16:26:02 +0300

    Add Ex009

---
+++ Ex009/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа находит большее число из трех групп чисел
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int[] array = {11, 25, 44, 56, 16, 48, 77, 87, 24};
+int result = Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
