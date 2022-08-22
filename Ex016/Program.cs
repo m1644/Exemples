5ecfa81b318269fd8d613656af141d0e55d6b811
MaxX <maxx-f@mail.ru>
2022-08-22 09:57:56 +0300

    Add Ex016

---
+++ Ex016/Program.cs
@@ -0,0 +1,25 @@
+﻿// Факториал!!!
+
+//double Factorial (int n)
+//{
+//    if(n == 1) return 1;
+//    else return n * Factorial(n - 1);
+//}
+//for (int i = 1; i < 40; i++)
+//{
+//    Console.WriteLine($"{i}! = {Factorial(i)}");
+//}
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
